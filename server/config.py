80269f92d6886e91ff82209d52447c4c5f7410a1
yeecin <92563070+yeecin@users.noreply.github.com>
2025-05-12 06:11:39 +0800

    Update config.py

--- server/config.py
+++ server/config.py
@@ -1,4 +1,4 @@
 class Config:
     # MySQL 数据库配置
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://jdbc:jdbc@120.76.99.179:3306/beisi'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://jdbc:jdbc@yourip:3306/beisi'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
